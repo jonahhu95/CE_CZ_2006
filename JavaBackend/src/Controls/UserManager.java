3e4923ead1d7b70f3d7a8722b7280b9a78216302
delphine <leehanwei0220@gmail.com>
2017-10-12 11:23:17 +0800

    no message

--- JavaBackend/src/Controls/UserManager.java
+++ JavaBackend/src/Controls/UserManager.java
@@ -1,7 +1,48 @@
 package Controls;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import Entities.User;
+
 /**
  * Created by jonah on 28/9/2017.
  */
 public class UserManager {
+	private static ArrayList<String> UserList= new ArrayList<String>();
+
+	public boolean createUser() {
+		//prompt for input
+		String username=null;
+		String password=null;
+		//
+		for(int i=0;i<UserList.size();i++) {
+			if(username==UserList.get(i))
+				return false;
+		}
+		DatabaseManager valid=new DatabaseManager();
+		boolean result= valid.createAccount(username, password);
+		return result;
+	}
+	
+	
+	 public static Connection getConnection() throws Exception{
+	    	//always check whether there is valid access to database
+	    	try {
+	    		String driver= "com.mysql.jdbc.Driver";
+	    		String url="jdbc:mysql://localhost:3306/job?autoReconnect=true&useSSL=false";
+	    		String username="root";
+	    		String password="041100";
+	    		Class.forName(driver);
+	    		
+	    		Connection con=DriverManager.getConnection(url,username,password);
+	    		System.out.println("Connected");
+	    		return con;
+	    	}catch(Exception e) {System.out.println(e);}
+	    	
+	    	return null;
+	    }
 }
