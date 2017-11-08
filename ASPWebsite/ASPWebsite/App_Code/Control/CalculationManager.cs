f67b0674eb6fe59fe866fdc445d38fac78dda39e
QingYao <jonahhu95@gmail.com>
2017-11-08 23:54:43 +0800

    .

--- ASPWebsite/ASPWebsite/App_Code/Control/CalculationManager.cs
+++ ASPWebsite/ASPWebsite/App_Code/Control/CalculationManager.cs
@@ -91,23 +91,8 @@ namespace ASPWebsite.App_Code.Control
 
         public Boolean getAddressValid(string address)
         {
-            try
-            {
-                String url = apiManager.generateCall_GetCoordinates(address);
-                var client = new RestClient(url);
-                var request = new RestRequest(Method.GET);
-                IRestResponse response = client.Execute(request);;
-                JObject obj = JObject.Parse(response.Content);
-                JArray ar = (JArray)obj["results"];
-                String formattedAddress = ar.First["formatted_address"].ToString();
-                Boolean contain = formattedAddress.Contains("Singapore");
-                if (contain)//return true for address in singapore
-                    return true;
-            }
-            catch (Exception ex)
-            {
+          
 
-<<<<<<< HEAD
             string url;
             JObject obj;
             try
@@ -124,8 +109,6 @@ namespace ASPWebsite.App_Code.Control
             catch (Exception ex)
             {
 
-=======
->>>>>>> 7ce8e04e1c3ce0388a436eb510ab1fe569792b51
             }
             return false;
             
